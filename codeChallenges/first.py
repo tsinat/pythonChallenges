5e03c933e8cc1795c06e931e6fd19495c8a2feb1
Tsinat Zeree <cheru87@gmail.com>
2017-03-10 12:14:19 -0800

    initial commit

---
+++ codeChallenges/first.py
@@ -0,0 +1,15 @@
+import os
+
+def firstFunction():
+    print('yeah');
+    print('this too!');
+
+firstFunction();
+
+# this is a comment in python
+# yeah this one too
+
+'''
+    this will be multiline comments in python
+    you can add multiple lines of comments here
+'''
